e460ab44057e1a7e392e76f5d009baf919c3728c
ashish sharma <ashish.sharma@abscod.com>
2022-08-30 18:02:34 -0700

    Redesign manage user page in table form

--- src/components/ManageUser.js
+++ src/components/ManageUser.js
@@ -1,27 +1,117 @@
 import React, { useState } from 'react'
-
+import AddNewUser from './AddNewUser'
+import SundarPichari from '../Assets/Sundar Pichai.jpg'
 export default function ManageUser() {
 
-    const [addUser, setAddUser] = useState({
-        name: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-        role: ""
-    })
-
-    const [deleteUser, setDeleteUser] = useState({
-        email: "",
-        name : "",
-        role: ""
-    })
+    // const [deleteUser, setDeleteUser] = useState({
+    //     email: "",
+    //     name: "",
+    //     role: ""
+    // })
 
 
     return (
         <>
             <div className='container'>
-                <div className='row pt-5 justify-content-evenly'>
-                    <div className='col-md-5 col-lg-4 neomorphism  p-3 ms-3 pt-2'>
+                <div className="row pt-3">
+                    <div className="col-md-6">
+                        <h3 className='text-center theme-color'>Users List</h3>
+                    </div>
+                    <div className="col-md-6">
+                        <AddNewUser />
+                        <button type="button" class="btn btn-outline-secondary rounded-pill w-100" data-bs-toggle="modal" data-bs-target="#addUser">Add New User</button>
+                    </div>
+                </div>
+                <div className='row pt-3'>
+                    <div>
+                        <div style={{ height: "67vh" }} className="table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead className='table-dark'>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Photo</th>
+                                        <th>User Name</th>
+                                        <th>Email Id</th>
+                                        <th>Role</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>1</th>
+                                        <td>
+                                            <div className='rounded shadow me-3' style={{ width: "50px", height: "50px" }}>
+                                                <img src={SundarPichari} alt="" className='w-100 h-100 overflow-hidden rounded ' />
+                                            </div>
+                                        </td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>Otto</td>
+                                        <td className='text-center'><button type="button" className='btn btn-outline-danger rounded-pill w-50'>Remove</button></td>
+                                    </tr>
+                                    <tr>
+                                        <th>2</th>
+                                        <td>
+                                            <div className='rounded shadow me-3' style={{ width: "50px", height: "50px" }}>
+                                                <img src={SundarPichari} alt="" className='w-100 h-100 overflow-hidden rounded ' />
+                                            </div>
+                                        </td>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                        <td className='text-center'><button type="button" className='btn btn-outline-danger rounded-pill w-50'>Remove</button></td>
+
+                                    </tr>
+                                    <tr>
+                                        <th>3</th>
+                                        <td>
+                                            <div className='rounded shadow me-3' style={{ width: "50px", height: "50px" }}>
+                                                <img src={SundarPichari} alt="" className='w-100 h-100 overflow-hidden rounded ' />
+                                            </div>
+                                        </td>
+                                        <td>Larry the Bird</td>
+                                        <td>Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td className='text-center'><button type="button" className='btn btn-outline-danger rounded-pill w-50'>Remove</button></td>
+
+                                    </tr>
+                                    <tr>
+                                        <th>4</th>
+                                        <td>
+                                            <div className='rounded shadow me-3' style={{ width: "50px", height: "50px" }}>
+                                                <img src={SundarPichari} alt="" className='w-100 h-100 overflow-hidden rounded ' />
+                                            </div>
+                                        </td>
+                                        <td>Larry the Bird</td>
+                                        <td>Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td className='text-center'><button type="button" className='btn btn-outline-danger rounded-pill w-50'>Remove</button></td>
+
+                                    </tr>
+                                    <tr>
+                                        <th>5</th>
+                                        <td>
+                                            <div className='rounded shadow me-3' style={{ width: "50px", height: "50px" }}>
+                                                <img src={SundarPichari} alt="" className='w-100 h-100 overflow-hidden rounded ' />
+                                            </div>
+                                        </td>
+                                        <td>Larry the Bird</td>
+                                        <td>Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td className='text-center'><button type="button" className='btn btn-outline-danger rounded-pill w-50'>Remove</button></td>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            {/* <div className='col-md-5 col-lg-4 neomorphism  p-3 ms-3 pt-2'>
                         <div>
                             <h4 className='text-center theme-color mb-4'>Add User</h4>
                         </div>
@@ -82,9 +172,7 @@ export default function ManageUser() {
                             <button type="button" className='btn theme-bg text-white rounded-pill me-5 text-nowrap'>Delete User</button>
                             <button type="button" className='btn btn-secondary rounded-pill'  onClick={()=>setDeleteUser({...deleteUser,name:"",email:"",role:""}) }>Cancel</button>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </div> */}
         </>
     )
 }
