97c40eb8434816138487c214d86ab9a94f1ed3cd
ashish sharma <ashish.sharma@abscod.com>
2022-09-03 15:39:31 -0700

    added context provided inside route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import WriteComment from './components/SelectedProject/Comments/WriteComment';
 import SelectedProject from './components/SelectedProject/SelectedProject';
+import WriteComment from './components/SelectedProject/Comments/WriteComment';
+import { ProjectContext } from './components/contexts/ProjectContext';
+import { useState } from 'react';
 
 function App() {
+  const [projectId, setProjectId] = useState(null);
 
   return (
-    <BrowserRouter>    
-      
+    <BrowserRouter>
+
+
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/project/:id' element={<SelectedProject/>} />
-        <Route path='/comment' element={<WriteComment/>} />
+        <Route path='/' element={
+          <ProjectContext.Provider value={{ projectId, setProjectId }}>
+            <Home setProjectId={setProjectId} />
+          </ProjectContext.Provider>
+        } />
+
+        <Route path='/project' element={
+          <ProjectContext.Provider value={{ projectId, setProjectId }}>
+            <SelectedProject projectId={projectId} />
+          </ProjectContext.Provider>
+        } />
+
+        <Route path='/comment' element={
+          <ProjectContext.Provider value={{ projectId, setProjectId }}>
+            <WriteComment projectId={projectId} />
+          </ProjectContext.Provider>
+        } />
+
         <Route path='*' element={<><h1 className='bg-danger text-white text-center mt-0'>404 : Page Not Found</h1></>} />
       </Routes>
     </BrowserRouter>
