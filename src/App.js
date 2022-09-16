db620ed0703cb9c3afb552c2f6ae14d3aa097a5e
ashish sharma <ashish.sharma@abscod.com>
2022-09-16 15:28:03 -0700

    added route protection by auth

--- src/App.js
+++ src/App.js
@@ -39,28 +39,36 @@ function App() {
     <BrowserRouter>
       <UserContext.Provider value={{ user, setUser }}>
         <Routes>
-
+        {!user?.auth && (
           <Route path='/' element={<Landing />} />
+        )}
 
-          <Route path='/main' element={
-            <ProjectContext.Provider value={{ projectId, setProjectId }}>
-              <Home setProjectId={setProjectId} />
-            </ProjectContext.Provider>
-          } />
 
-          <Route path='/project' element={
-            <ProjectContext.Provider value={{ projectId, setProjectId }}>
-              <SelectedProject projectId={projectId} />
-            </ProjectContext.Provider>
-          } />
+          {user?.auth && user?.userData && (
+            <>
+              <Route path='/' element={<Landing />} />
 
-          <Route path='/comment' element={
-            <ProjectContext.Provider value={{ projectId, setProjectId }}>
-              <WriteComment projectId={projectId} />
-            </ProjectContext.Provider>
-          } />
+              <Route path='/main' element={
+                <ProjectContext.Provider value={{ projectId, setProjectId }}>
+                  <Home setProjectId={setProjectId} />
+                </ProjectContext.Provider>
+              } />
 
+              <Route path='/project' element={
+                <ProjectContext.Provider value={{ projectId, setProjectId }}>
+                  <SelectedProject projectId={projectId} />
+                </ProjectContext.Provider>
+              } />
+
+              <Route path='/comment' element={
+                <ProjectContext.Provider value={{ projectId, setProjectId }}>
+                  <WriteComment projectId={projectId} />
+                </ProjectContext.Provider>
+              } />
+            </>
+          )}
           <Route path='*' element={<><h1 className='bg-danger text-white text-center mt-0'>404 : Page Not Found</h1></>} />
+
 
         </Routes>
       </UserContext.Provider>
