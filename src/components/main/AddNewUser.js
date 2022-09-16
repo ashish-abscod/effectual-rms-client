11722d653f242a57980e0336587feb0b88343510
ashish sharma <ashish.sharma@abscod.com>
2022-09-15 17:10:08 -0700

    upload profile on cloudinary

--- src/components/main/AddNewUser.js
+++ src/components/main/AddNewUser.js
@@ -1,77 +1,215 @@
-import { useState } from 'react'
-export default function AddNewUser() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+
+export default function AddNewUser() {
     const [addUser, setAddUser] = useState({
         name: "",
         email: "",
         password: "",
         confirmPassword: "",
-        role: "Patent Expert",
-        profile: null
-    })
+        role: "",
+        picture: "",
+    });
+    const [file, setFile] = useState();
+
+    const uploadSingleFile = (e) => {
+        if (e.target.files[0]) {
+            // console.log("e.target.files[0]: ", e.target.files[0]);
+            const reader = new FileReader();
+            setFile(URL.createObjectURL(e.target.files[0]));
+            reader.readAsDataURL(e.target.files[0]);
+            reader.onloadend = () => {
+                // console.log("reader.result: ", reader.result);
+                setAddUser({ ...addUser, ["picture"]: reader.result });
+                setFile(reader.result);
+            };
+        }
+    };
+
+    useEffect(() => {
+        // console.log("file: ", file);
+    }, [file]);
+
+    const submitData = async () => {
+        try {
+            const response = await axios.post("http://localhost:8080/users", addUser);
+
+            console.log("addresponse: ", response);
+        } catch (error) {
+            console.log("error: ", error.response);
+            alert(error.response.data.error);
+        }
+    };
 
     return (
         <>
-            <div className="modal fade" id="addUser" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-hidden="true">
+            <div
+                className="modal fade"
+                id="addUser"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabIndex="-1"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title text-center theme-color" id="addUserLabel">Add New User</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5
+                                className="modal-title text-center theme-color"
+                                id="addUserLabel"
+                            >
+                                Add New User
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
                         </div>
                         <div className="modal-body">
                             <div className="container">
                                 <div className="row">
-                                    <div className='col-md-8'>
+                                    <div className="col-md-8">
                                         <div className="input-field mt-0 ">
-                                            <input type="text" className="form-control" required value={addUser.name} onChange={(e) => setAddUser({ ...addUser, name: e.target.value })} />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="inputPassword4"
+                                                required
+                                                value={addUser.name}
+                                                onChange={(e) =>
+                                                    setAddUser({ ...addUser, name: e.target.value })
+                                                }
+                                            />
                                             <label>Name:</label>
-                                            <span className='d-none'>Error : Field Required</span>
+                                            <span className="d-none">Error : Field Required</span>
                                         </div>
                                         <div className="input-field">
-                                            <input type="email" className="form-control" required value={addUser.email} onChange={(e) => setAddUser({ ...addUser, email: e.target.value })} />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="inputPassword4"
+                                                required
+                                                value={addUser.email}
+                                                onChange={(e) =>
+                                                    setAddUser({ ...addUser, email: e.target.value })
+                                                }
+                                            />
                                             <label>Email:</label>
-                                            <span className='d-none'>Error : Field Required</span>
+                                            <span className="d-none">Error : Field Required</span>
                                         </div>
                                         <div className="input-field">
-                                            <input type="password" className="form-control" required value={addUser.password} onChange={(e) => setAddUser({ ...addUser, password: e.target.value })} />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="inputPassword4"
+                                                required
+                                                value={addUser.password}
+                                                onChange={(e) =>
+                                                    setAddUser({ ...addUser, password: e.target.value })
+                                                }
+                                            />
                                             <label>Password:</label>
-                                            <span className='d-none'>Error : Field Required</span>
+                                            <span className="d-none">Error : Field Required</span>
                                         </div>
                                         <div className="input-field">
-                                            <input type="password" className="form-control" required value={addUser.confirmPassword} onChange={(e) => setAddUser({ ...addUser, confirmPassword: e.target.value })} />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="inputPassword4"
+                                                required
+                                                value={addUser.confirmPassword}
+                                                onChange={(e) =>
+                                                    setAddUser({
+                                                        ...addUser,
+                                                        confirmPassword: e.target.value,
+                                                    })
+                                                }
+                                            />
                                             <label>Confirm Password:</label>
-                                            <span className='d-none'>Error : Field Required</span>
+                                            <span className="d-none">Error : Field Required</span>
                                         </div>
                                         <div className="input-field">
-                                            <select className="form-select" required value={addUser.role} onChange={(e) => setAddUser({ ...addUser, role: e.target.value })}>
-                                                <option value="Patent Expert">Patent Expert</option>
-                                                <option value="Technical Expert">Technical Expert</option>
-                                                <option value="Client Admin">Client Admin</option>
-                                            </select>
-                                            <label>X/Y/A:</label>
-                                            <span className='d-none'>Error : Field Required</span>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="inputPassword4"
+                                                required
+                                                value={addUser.role}
+                                                onChange={(e) =>
+                                                    setAddUser({ ...addUser, role: e.target.value })
+                                                }
+                                            />
+                                            <label>Role:</label>
+                                            <span className="d-none">Error : Field Required</span>
                                         </div>
                                     </div>
-                                    <div className='col-md-4 mt-4'>
-                                        <input type="file" className='form-control' onChange={(e) => setAddUser({ ...addUser, profile: e.target.files[0] })} />
-                                        <div className='mt-3'>
-                                            {addUser.profile && <img src={URL.createObjectURL(addUser.profile)} alt="" className='mw-100' />}
+                                    <div className="col-md-4 mt-4">
+                                        <div className="mt-3">
+                                            <label
+                                                htmlFor="exampleFormControlInput1"
+                                                className="form-label"
+                                            >
+                                                Choose image
+                                            </label>
+                                            <input
+                                                // disabled={loader}
+                                                type="file"
+                                                // disabled={file || loader}
+                                                className="form-control"
+                                                onChange={uploadSingleFile}
+                                            />
+                                            <img
+                                                src={file && file}
+                                                alt="dummy"
+                                                height="200px"
+                                                width="200px"
+                                            />
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-footer d-flex justify-content-between">
-                            <button type="button" className="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Close</button>
-                            <div className=''>
-                                <button type="button" className='btn btn-secondary rounded-pill me-3' onClick={() => setAddUser({ ...addUser, name: "", email: "", password: "", confirmPassword: "", role: "" })}>Clear</button>
-                                <button type="button" className='btn theme-bg text-white rounded-pill'>Add User</button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary rounded-pill"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <div className="">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary rounded-pill me-3"
+                                    onClick={() =>
+                                        setAddUser({
+                                            ...addUser,
+                                            name: "",
+                                            email: "",
+                                            password: "",
+                                            confirmPassword: "",
+                                            role: "",
+                                            picture: "",
+                                        })
+                                    }
+                                >
+                                    Clear
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn theme-bg text-white rounded-pill"
+                                    onClick={submitData}
+                                >
+                                    Add User
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
