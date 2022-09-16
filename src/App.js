92a1f2fdc18d4e37a1ea832632330ab1864116b4
ashish sharma <ashish.sharma@abscod.com>
2022-09-15 17:05:56 -0700

    using auth in app.js

--- src/App.js
+++ src/App.js
@@ -4,38 +4,66 @@ import Home from './components/main/Home';
 import SelectedProject from './components/SelectedProject/SelectedProject';
 import WriteComment from './components/SelectedProject/Comments/WriteComment';
 import { ProjectContext } from './components/contexts/ProjectContext';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Landing from './components/landing/Landing';
+import { UserContext } from './components/contexts/userContext'
 
 function App() {
   const [refId, setRefId] = useState(null);
 
+  const [user, setUser] = useState({ userData: "", auth: false, token: null });
+
+  useEffect(() => {
+    console.log("user: ", user);
+  }, [user]);
+
+  useEffect(() => {
+    if (user?.auth) {
+      setUser({
+        ...user,
+        ["auth"]: true,
+        ["userData"]: JSON.parse(localStorage.getItem("userData")),
+        ["token"]: JSON.parse(localStorage.getItem("token")),
+      });
+    } else if (localStorage.getItem("userData")) {
+      setUser({
+        ...user,
+        ["auth"]: true,
+        ["userData"]: JSON.parse(localStorage.getItem("userData")),
+        ["token"]: JSON.parse(localStorage.getItem("token")),
+      });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
 
-      <Routes>
-        <Route path='/' element={<Landing />} />
+          <Route path='/' element={<Landing />} />
 
-        <Route path='/main' element={
-          <ProjectContext.Provider value={{ refId, setRefId }}>
-            <Home setRefId={setRefId} />
-          </ProjectContext.Provider>
-        } />
+          <Route path='/main' element={
+            <ProjectContext.Provider value={{ refId, setRefId }}>
+              <Home setRefId={setRefId} />
+            </ProjectContext.Provider>
+          } />
 
-        <Route path='/project' element={
-          <ProjectContext.Provider value={{ refId, setRefId }}>
-            <SelectedProject refId={refId} />
-          </ProjectContext.Provider>
-        } />
+          <Route path='/project' element={
+            <ProjectContext.Provider value={{ refId, setRefId }}>
+              <SelectedProject refId={refId} />
+            </ProjectContext.Provider>
+          } />
 
-        <Route path='/comment' element={
-          <ProjectContext.Provider value={{ refId, setRefId }}>
-            <WriteComment refId={refId} />
-          </ProjectContext.Provider>
-        } />
+          <Route path='/comment' element={
+            <ProjectContext.Provider value={{ refId, setRefId }}>
+              <WriteComment refId={refId} />
+            </ProjectContext.Provider>
+          } />
 
-        <Route path='*' element={<><h1 className='bg-danger text-white text-center mt-0'>404 : Page Not Found</h1></>} />
-      </Routes>
+          <Route path='*' element={<><h1 className='bg-danger text-white text-center mt-0'>404 : Page Not Found</h1></>} />
+
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
