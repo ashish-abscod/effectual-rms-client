13d454396bd977d6a83c878ce6a83f0a8117e9c1
ashish-abscod <ashish.sharma@abscod.com>
2022-09-21 14:39:31 +0530

    Merge branch 'tamanna' of https://github.com/ashish-abscod/effectual-rms-client

--- src/components/Forms/CreateProject.js
+++ src/components/Forms/CreateProject.js
@@ -175,7 +175,7 @@ export default function CreateProject() {
           </div>
         </div>
 
-        <div className="row gy-3 gy-md-3 gx-4 mt-3 row-cols-lg-3 row-cols-md-2 justify-content-evenly">
+        <div>
           {PageDisplay()}
         </div>
       </div>
