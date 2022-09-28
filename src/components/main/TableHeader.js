3ba5d766cfa07a3b3a511378b2ab817d8ec00d82
ashish-abscod <ashish.sharma@abscod.com>
2022-09-28 14:43:36 +0530

    changed placeholder

--- src/components/main/TableHeader.js
+++ src/components/main/TableHeader.js
@@ -6,7 +6,7 @@ export default function TableHeader({props}) {
         <>
             <div className='d-flex justify-content-between mt-2 px-3 py-1'>
                 <div>
-                    <input type="search" placeholder="Quick Search Here..." className="form-control me-3" onChange={(e) => props.setSearch(e.target.value)} />
+                    <input type="search" placeholder="Quick Advance Search..." className="form-control me-3" onChange={(e) => props.setSearch(e.target.value)} />
                 </div>
                 <div>
                     <CSVLink data={props?.selectedProjects} filename={"Effectual-RMS-Projects.csv"} className="btn btn-success text-light me-3" type='button'>Bibliography</CSVLink>
