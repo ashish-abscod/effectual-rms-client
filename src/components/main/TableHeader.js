c41a63a842181ac3d086c9a45bd4bce334c1bb0e
ashish sharma <ashish.sharma@abscod.com>
2022-10-08 10:42:51 +0530

    fixed header style in projects Table

--- src/components/main/TableHeader.js
+++ src/components/main/TableHeader.js
@@ -5,12 +5,12 @@ export default function TableHeader({props}) {
     return (
         <>
             <div className='d-flex justify-content-between mt-2 px-3 py-1'>
-                <div>
-                    <input type="search" placeholder="Quick Advance Search..." className="form-control me-3" onChange={(e) => props.setSearch(e.target.value)} />
+                <div className="w-50">
+                    <input type="search" placeholder="Quick Search" className="form-control me-3" onChange={(e) => props.setSearch(e.target.value)} />
                 </div>
                 <div>
-                    <CSVLink data={props?.selectedProjects} filename={"Effectual-RMS-Projects.csv"} className="btn btn-success text-light me-3" type='button'>Bibliography</CSVLink>
-                    <CSVLink data={props?.projects} filename={"Effectual-RMS-Projects.csv"} className="btn theme-bg text-light" type='button'>Bibliography All</CSVLink>
+                    <CSVLink data={props?.selectedProjects} filename={"Effectual-RMS-Projects.csv"} className="btn btn-success text-light me-3 px-4" type='button'>Bibliography</CSVLink>
+                    <CSVLink data={props?.projects} filename={"Effectual-RMS-Projects.csv"} className="btn theme-bg text-light px-4" type='button'>Bibliography All</CSVLink>
                 </div>
             </div>
         </>
