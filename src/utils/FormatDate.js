1732de10767e393c0928ebc41d20abe6fd18b814
ashish sharma <ashish.sharma@abscod.com>
2022-10-14 18:01:31 +0530

    added a formatedDateTime utility

--- src/utils/FormatDate.js
+++ src/utils/FormatDate.js
@@ -1,6 +1,23 @@
 //get formated date by providing a raw date
-export const getFormatedToday = (value) => {
-    var date = new Date(value);
-    var str = date.getFullYear() + "-" + (date.getMonth()<9 ?  `0${date.getMonth()+1}` : date.getMonth()+1) + "-" + (date.getDate()<10 ? `0${date.getDate()}` : date.getDate());
+export const getFormatedDate = () => {
+    var date = new Date();
+    var str = date.getFullYear() + "-" + (date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1) + "-" + (date.getDate() < 10 ? `0${date.getDate()}` : date.getDate());
     return str;
 }
+
+export const getFormatedDateTime = () => {
+    var date = new Date();
+    var str =
+        date.getFullYear() +
+        "-" +
+        (date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1) +
+        "-" +
+        (date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()) +
+        " " +
+        (date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()) +
+        ":" +
+        (date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()) +
+        ":" +
+        (date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds());
+    return str;
+};
