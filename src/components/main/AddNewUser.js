7ef93872c8dc2c45db48c415667398fbdd3a4b20
Bandana <bandanasatpathy435@gmail.com>
2022-10-22 15:01:47 +0530

    taken pull from main

--- src/components/main/AddNewUser.js
+++ src/components/main/AddNewUser.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useNavigate } from 'react-router-dom';
-
 export default function AddNewUser() {
+
   const [addUser, setAddUser] = useState({
     name: "",
     email: "",
