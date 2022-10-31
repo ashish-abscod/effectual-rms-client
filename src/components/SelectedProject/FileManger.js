67d31c38d457e347623fccc117c1d5acb02372eb
ashish sharma <ashish.sharma@abscod.com>
2022-10-31 17:39:35 +0530

    added a functionality of report uploadation by effectual admin

--- src/components/SelectedProject/FileManger.js
+++ src/components/SelectedProject/FileManger.js
@@ -4,17 +4,20 @@ import { ProjectContext } from '../contexts/ProjectContext';
 import axios from "axios"
 import { BiDownload } from "react-icons/bi"
 import Moment from 'react-moment';
-import {FiUpload} from "react-icons/fi";
+import { FiUpload } from "react-icons/fi";
+import ReportUpload from './ReportUpload';
+import { UserContext } from '../contexts/UserContext';
 
 export default function FileManger() {
-
-    const [effectualReports, setEffectualReports] = useState([])
-    const [clientReports, setClientReports] = useState([])
+    const { user } = useContext(UserContext);
+    const [effectualReports, setEffectualReports] = useState([]);
+    const [clientReports, setClientReports] = useState([]);
     const { setProjectId, projectId } = useContext(ProjectContext);
+
     useEffect(() => {
         if (!projectId) setProjectId(window?.localStorage?.getItem('projectId'));
-      }, [projectId,setProjectId])
-    
+    }, [projectId, setProjectId])
+
     useEffect(() => {
         const getEffectualReports = async () => {
             try {
@@ -34,19 +37,19 @@ export default function FileManger() {
             try {
                 const info = await axios.get(`http://localhost:8080/commentFiles/client/${projectId}`)
                 setClientReports(info?.data?.result);
-
             } catch (error) {
                 console.log(error);
             }
         }
         getClientReports();
-    }, [projectId])
-   
+    }, [projectId]);
+
+
     return (
         <>
             <div className='container'>
-                <div className='row row-cols-md-2 pt-4 '>
-                    <div>
+                <div className='row row-cols-md-2 pt-3 '>
+                    <div className='mt-2'>
                         <h5 className='text-center theme-color fw-bold'>
                             Client Files
                         </h5>
@@ -66,12 +69,10 @@ export default function FileManger() {
                                             <tr key={`${a}${b}`}>
                                                 <td>{item.uploadedBy}</td>
                                                 <td><Moment format="DD/MM/YYYY HH:mm a">{item.createdAt}</Moment></td>
-                                                <td className='text-center'><a href={url} className="me-3 fw-bold lh-1"><BiDownload size={25}/></a></td>
+                                                <td className='text-center'><a href={url} className="me-3 fw-bold lh-1"><BiDownload size={25} /></a></td>
                                             </tr>
-
                                         )
                                     )
-
                                     }
 
                                 </tbody>
@@ -80,10 +81,15 @@ export default function FileManger() {
                     </div>
 
                     <div>
-                        <h5 className='text-center theme-color fw-bold d-inline-block   '>
-                            Effectual Files
-                        </h5>
-                        <button type="button" className='btn btn-primary lh-1 ms-1'><FiUpload/></button>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <h5 className='theme-color fw-bold d-inline-block'>
+                                Effectual Files
+                            </h5>
+                            {user?.userData?.role === "Effectual Admin" ?
+                                <button type="button" className='btn btn-primary mb-2 ms-3 btn-sm' data-bs-toggle="modal" data-bs-target="#reportUpload"><FiUpload /> Upload Report</button>
+                                : ""
+                            }
+                        </div>
                         <div style={{ height: "70vh" }} className="table-responsive">
                             <table className="table table-bordered table-striped">
                                 <thead className='table-dark'>
@@ -95,26 +101,27 @@ export default function FileManger() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                               
+
                                     {effectualReports?.map?.((item, a) =>
                                         item?.files?.map((url, b) =>
                                             <tr key={`${a}${b}`}>
                                                 <td>{item.uploadedBy}</td>
                                                 <td><Moment format="DD/MM/YYYY HH:mm a">{item.createdAt}</Moment></td>
-                                                <td className='text-center'><a href={url} className="me-3 fw-bold lh-1"><BiDownload size={25}/></a></td>
+                                                <td className='text-center'><a href={url} className="me-3 fw-bold lh-1"><BiDownload size={25} /></a></td>
                                             </tr>
-
                                         )
                                     )
-
                                     }
-
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
             </div>
+            {user?.userData?.role === "Effectual Admin" ?
+                <ReportUpload />
+                : ""
+            }
         </>
     )
 }
---
+++ src/components/SelectedProject/ReportUpload.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { toast } from 'react-toastify';
+import { ProjectContext } from '../contexts/ProjectContext';
+import { UserContext } from '../contexts/UserContext';
+
+export default function ReportUpload() {
+
+
+    const { user } = useContext(UserContext);
+    const { projectId } = useContext(ProjectContext);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [reportStatus, setReportStatus] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [resource, setResource] = useState({ file: "", filename: "", emailContent: "", projectId: projectId, role: user?.userData?.role, uploadedBy: user?.userData?.name });
+
+    useEffect(() => {
+        setResource((preRes)=> ({ ...preRes, projectId: projectId }));
+    }, [projectId]);
+
+
+    const uploadSingleFile = (e) => {
+        if (e.target.files[0]) {
+            const filename = e.target.files[0].name;
+            const reader = new FileReader();
+            reader.readAsDataURL(e.target.files[0]);
+            reader.onloadend = () => {
+                setResource({ ...resource, file: reader.result, filename: filename });
+            };
+        }
+    };
+
+
+    const handleReportUpload = async () => {
+        try {
+            setIsLoading(true);
+            setIsDisabled(true);
+            const res1 = await axios.put(`${process.env.REACT_APP_API_URL}/files/updateStatus/${projectId}`, { status: reportStatus });
+            if (res1?.data?.status === "success") toast.success(res1?.data?.msg);
+            else toast.error(res1?.data?.msg)
+            const res2 = await axios.post(`${process.env.REACT_APP_API_URL}/files/uploadReportAndSendEmails`, resource);
+            if (res2?.data?.status === "success") toast.success(res2?.data?.msg);
+            else toast.error(res2?.data?.msg);
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong.");
+        } finally {
+            setIsLoading(false);
+            setIsDisabled(false);
+        }
+    }
+    return (
+        <>
+            <div
+                className="modal fade"
+                id="reportUpload"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabIndex="-1"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title text-center theme-color">
+                                Upload Project Report
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <label className="text-primary fw-bold d-flex align-items-center">
+                                            Upload Report:
+                                            {isLoading && (
+                                                <>
+                                                    <div className="spinner-border text-secondary ms-3 me-2" role="status">
+                                                        <span className="sr-only"></span>
+                                                    </div>
+                                                    <span className="text-secondary">Uploading...</span>
+                                                </>
+                                            )}
+                                        </label>
+                                        <input
+                                            type="file"
+                                            className="form-control mt-2 mb-3"
+                                            onChange={(e) => uploadSingleFile(e)}
+                                            disabled={isDisabled}
+                                        />
+                                        <label>Attach Email:</label>
+                                        <input
+                                            type="checkbox"
+                                            className="form-check-input ms-3"
+                                            value={isChecked}
+                                            onChange={(e) => setIsChecked(!isChecked)}
+                                        />
+                                        {isChecked ?
+                                            <div className="input-field">
+                                                <textarea
+                                                    className="form-control"
+                                                    placeholder='Write something'
+                                                    value={resource?.emailContent}
+                                                    onChange={(e) => setResource({ ...resource, emailContent: e.target.value })}
+                                                />
+                                            </div>
+                                            : ""
+                                        }
+                                        <div className="input-field">
+                                            <select
+                                                className="form-select"
+                                                value={reportStatus}
+                                                onChange={(e) => { setReportStatus(e.target.value) }}
+                                            >
+                                                <option value="Initial Report">Initial Report</option>
+                                                <option value="Interim Report">Interim Report</option>
+                                                <option value="Final Report">Final Report</option>
+                                            </select>
+                                            <label>Report Status:</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="modal-footer d-flex justify-content-between">
+                            <button
+                                type="button"
+                                className="btn btn-secondary rounded-pill"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button
+                                type="button"
+                                disabled={isDisabled}
+                                variant="btn btn-success w-100"
+                                className="btn theme-bg text-white rounded-pill"
+                                onClick={handleReportUpload}
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
